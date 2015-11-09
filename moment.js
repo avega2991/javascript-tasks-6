20cd7ca68c77f1faff81aca8745c5925196d5dd4
avega2991 <avega2991@github.com>
2015-11-09 23:52:54 +0500

    Initial

--- moment.js
+++ moment.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var daySeconds = 60 * 60 * 24;
+var hourSeconds = 60 * 60;
+var hourRegex = /([0-9]{2}:[0-9]{2})/;
+
 module.exports = function () {
     return {
         // Здесь как-то хранится дата ;)
@@ -10,11 +14,63 @@ module.exports = function () {
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            var day = this.date.split(' ')[0];
+            var time = this.date.match(hourRegex)[0];
+            var result = pattern.replace('%DD', day);
+            result = result.replace('%HH:%MM', getTime(time, this.timezone));
+            return result;
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
         // в человекопонятном виде
         fromMoment: function (moment) {
+            var verbose = declineWord(2, ['остался', 'осталось', 'осталось']) + ' ';
+            var days = '';
+            var hours = '';
+            var minutes = '';
+            var diff = getSeconds(this.date) - getSeconds(moment.date);
+            var daysCount = parseInt(diff / daySeconds);
+            if (daysCount >= 1) {
+                diff -= daysCount * daySeconds;
+                verb = declineWord(daysCount, ['остался', 'осталось', 'осталось']) + ' ';
+                days = daysCount + ' ';
+                days += declineWord(daysCount, ['день', 'дня', 'дней']) + ' ';
+            }
+            var hoursCount = parseInt(diff / hourSeconds);
+            if (hoursCount >= 1) {
+                diff -= hoursCount * hourSeconds;
+                hours = hoursCount + ' ';
+                hours += declineWord(hoursCount, ['час', 'часа', 'часов']) + ' ';
+            }
+            var minutesCount = parseInt(diff / 60);
+            if (minutesCount != 0) {
+                minutes = minutesCount + ' ';
+                minutes += declineWord(minutesCount, ['минута', 'минуты', 'минут']) + ' ';
+            }
+            return 'До ограбления ' + verbose + days + hours + minutes;
         }
     };
 };
+
+function getTime(time, timezone) {
+    var hour = time.split(':')[0];
+    var timezoneHours = hour - (-1) * timezone;
+    return timezoneHours + ':' + time.split(':')[1];
+}
+
+function declineWord(number, decls) {
+    var cases = [2, 0, 1, 1, 1, 2];
+    return decls[ (number % 100 > 4 && number % 100 < 20) ?
+        2 : cases[ (number % 10 < 5) ? number % 10 : 5] ];
+}
+
+function getSeconds(date) {
+    var time = date.match(hourRegex);
+    var data = {
+        day: date.split(' ')[0],
+        hour: parseInt(time[0].split(':')[0]),
+        minutes: parseInt(time[0].split(':')[1])
+    };
+    var daysPassed = data.day === 'ПН' ? 0 : data.day === 'ВТ' ? 1 : 2;
+    return daysPassed * daySeconds + data.hour * hourSeconds + data.minutes * 60;
+}
--- robbery.js
+++ robbery.js
@@ -7,9 +7,20 @@ module.exports.getAppropriateMoment = function (json, minDuration, workingHours)
     var appropriateMoment = moment();
 
     // 1. Читаем json
+    var jsonData = JSON.parse(json);
+    var bankSchedule = getBankSchedule(workingHours);
+    var normData = normalize(jsonData);
+    var normSchedule = normalize(bankSchedule);
     // 2. Находим подходящий ближайший момент начала ограбления
+    var fulltimeRange = {'from': 0, 'to': 3 * 24 * 60};
+    var allBusyRanges = getBusyRanges(normData);
+    var teamFreetime = substractRanges(fulltimeRange, allBusyRanges);
+    var robberyTimeArray = getCompatibleTime(normSchedule['Банк'], teamFreetime);
+    var robberyTime = getOptimalTime(robberyTimeArray, minDuration);
     // 3. И записываем в appropriateMoment
-
+    var timezone = workingHours.from.slice(-2);
+    appropriateMoment.date = getDate(robberyTime.from, timezone);
+    appropriateMoment.timezone = timezone;
     return appropriateMoment;
 };
 
@@ -22,3 +33,126 @@ module.exports.getStatus = function (moment, robberyMoment) {
 
     return 'Ограбление уже идёт!';
 };
+
+function getDate(utcMinutes, timezone) {
+    var daysOfWeek = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+    var dayIndex = Math.floor(utcMinutes / (24 * 60));
+    var day = daysOfWeek[dayIndex];
+    utcMinutes -= dayIndex * 24 * 60;
+    var hour = Math.floor(utcMinutes / 60);
+    utcMinutes -= hour * 60;
+    hour = numberToString(hour, 2);
+    utcMinutes = numberToString(utcMinutes, 2);
+    return day + ' ' + hour + ':' + utcMinutes + timezone;
+}
+
+function numberToString(number, numCount) {
+    --numCount;
+    return number < 10 * numCount ? '0' + number : number;
+}
+
+function getBankSchedule(bankTime) {
+    var days = ['ПН', 'ВТ', 'СР'];
+    return {'Банк': days.map(function (dayName) {
+        return {'from': dayName + ' ' + bankTime.from, 'to': dayName + ' ' + bankTime.to};
+    })};
+}
+
+function normalize(data) {
+    var newData = {};
+    for (var personName in data) {
+        var personData = data[personName];
+        personData = personData.reduce(function (acc, time) {
+            return acc.concat(dateRangeToUTC(time));
+        },
+        []);
+        newData[personName] = personData;
+    }
+    //console.log("NEW DATA: ", newData);
+    return newData;
+}
+
+function dateRangeToUTC(dateRange) {
+    var newTime = {};
+    newTime.from = parseToMinutesUTC(dateRange.from);
+    newTime.to = parseToMinutesUTC(dateRange.to);
+    return newTime;
+}
+
+function parseToMinutesUTC(time) {
+    var daysOfWeek = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+    var timezone = parseInt(time.slice(-2));
+    var day = daysOfWeek.indexOf(time.slice(0, 2));
+    var hours = parseInt(time.slice(3, 5)) - timezone;
+    var minutes = parseInt(time.slice(6, 8));
+
+    var minutesUTC = minutes + hours * 60 + day * 24 * 60;
+    return minutesUTC;
+}
+
+function substractRanges(minuend, subtraArray) {
+    var result = [];
+    var closestBusy = minuend;
+    while (true) {
+        closestBusy = minuend;
+        var minFrom = minuend.to;
+        var index = 0;
+        var closestIndex = index;
+        subtraArray.forEach(function (range) {
+            if (range.from >= minuend.from && range.from < minFrom) {
+                closestBusy = range;
+                closestIndex = index;
+                minFrom = closestBusy.from;
+            }
+            ++index;
+        });
+        if (closestBusy != minuend) {
+            result.push({'from': minuend.from, 'to': closestBusy.from});
+        }
+        result.splice(closestIndex, 1);
+        minuend.from = closestBusy.to;
+        if (closestBusy.to == minuend.to) {
+            break;
+        }
+    }
+    return result;
+}
+
+function getBusyRanges(dict) {
+    var result = [];
+    for (var elem in dict) {
+        var busyTime = dict[elem];
+        busyTime.forEach(function (time) {
+            result.push(time);
+        });
+    }
+    return result;
+}
+
+function getCompatibleTime(fstData, sndData) {
+    var freeTimeSpaces = [];
+    fstData.forEach(function (fstRange) {
+        sndData.forEach(function (sndRange) {
+            if (isCompatible(fstRange, sndRange)) {
+                var maxFrom = Math.max(fstRange.from, sndRange.from);
+                var minTo = Math.min(fstRange.to, sndRange.to);
+                freeTimeSpaces.push({'from': maxFrom, 'to': minTo});
+            }
+        });
+    });
+    return freeTimeSpaces;
+}
+
+function getOptimalTime(freetimeArray, minDuration) {
+    for (var i = 0; i < freetimeArray.length; ++i) {
+        var range = freetimeArray[i];
+        if (range.to - range.from >= minDuration) {
+            return range;
+        }
+    }
+    return [];
+}
+
+function isCompatible(first, second) {
+    return first.to >= second.from && second.to >= first.from;
+}
